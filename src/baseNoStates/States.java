bd049883f5e3638c5ba2e5db8003b709250bfa70
xavi-burgos99 <xavi.burgos99@gmail.com>
2024-10-24 00:28:28 +0200

    Unlocked Shortly

--- src/baseNoStates/States.java
+++ src/baseNoStates/States.java
@@ -4,5 +4,6 @@ public final class States {
   // possible actions in reader and area requests
   public static final String LOCKED = "locked";
   public static final String UNLOCKED = "unlocked";
+  public static final String UNLOCKED_SHORTLY = "unlocked_shortly";
   public static final String PROPPED = "propped";
 }
